a9b97f3c150f085df90eee3f500957f69e259374
it21293030 <105879757+himara1027@users.noreply.github.com>
2024-11-16 14:51:44 +0530

    Folder structure initial commit

---
+++ Client/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          I AM SENU
+        
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
