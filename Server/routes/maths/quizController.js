c747be744e3c84aec11844ebb808e2a80f7cca8b
it21293030 <105879757+himara1027@users.noreply.github.com>
2024-11-20 21:18:27 +0530

    modet train

--- Server/routes/maths/quizController.js
+++
@@ -1,9 +0,0 @@
-// src/routes/questionRoutes.js
-import express from 'express';
-import { generateQuiz } from '../../controllers/maths/quizController.js';
-
-const router = express.Router();
-
-router.get('/quiz/:category/:difficulty', generateQuiz);
-
-export default router;
